123094c4462693a8ca094305a71c242a9907e7d0
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2017-03-25 00:06:26 -0400

    get rid of unnecessary contents

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,9 +1,5 @@
-var bcrypt = require('bcryptjs');
 var express = require('express');
 var router  = express.Router();
-var mysql = require('mysql')
-var connection = require('../config/connection.js')
-
 
 //this is the users_controller.js file
 router.get('/', function(req,res) {
